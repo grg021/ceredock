0c2fd17dd42c193ce5d2477d04e047cac47e4f64
Greg Hermo <greg.hermo@gmail.com>
2016-11-08 11:12:29 +0800

    added cancel process queue

--- start-production.sh
+++ start-production.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose -f production-docker-compose.yml up -d caddy mysql mailhog pm2 redis
+docker-compose -f production-docker-compose.yml up -d nginx mysql mailhog pm2 redis
